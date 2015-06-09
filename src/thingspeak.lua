a44375aefb9c3266432dbb43adc17b4ba3416ac2
Michael Lucas <sla.erop@gmail.com>
2015-06-09 21:36:03 +0200

    Optimized thingspeak module

--- src/thingspeak.lua
+++ src/thingspeak.lua
@@ -13,17 +13,22 @@ conn:on("connection", function(conn)
         print("Connection succeeded")
         print("Sending data...")
     end
-    
+
+    conn:send("GET http://api.thingspeak.com/update?key="..APIkey.."&field1="..(temp/10).."&field2="..(hum/10))
     if luxSensor == "bh1750" then
-        conn:send("GET http://api.thingspeak.com/update?key="..APIkey.."&field1="..(temp/10).."&field2="..(hum/10).."&field3="..(lux0).." HTTP/1.1\r\n")
+        conn:send("&field3="..(lux0).." HTTP/1.1\r\n")
     end
     if luxSensor == "tsl2561" then
-        conn:send("GET http://api.thingspeak.com/update?key="..APIkey.."&field1="..(temp/10).."&field2="..(hum/10).."&field3="..(lux0).."&field4="..(lux1).." HTTP/1.1\r\n")
+        conn:send("&field3="..(lux0).."&field4="..(lux1).." HTTP/1.1\r\n")
     end
+    print("GET http://api.thingspeak.com/update?key="..APIkey.."&field1="..(temp/10).."&field2="..(hum/10).."&field3="..(lux0).."&field4="..(lux1).." HTTP/1.1")
     conn:send("Host: api.thingspeak.com\r\n")
-    conn:send("Accept: */*\r\n") 
+    print("Host: api.thingspeak.com")
+    conn:send("Accept: */*\r\n")
+    print("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua;)")
+    conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua;)\r\n")
+    print("Accept: */*")
     conn:send("\r\n")
-
 end)
 conn:on("receive", function(conn, payload)
     if(outputToSerial) then print("Closing connection...") end
@@ -40,5 +45,9 @@ conn:on("disconnection", function(conn)
             print("Failed to send data.");
         end
     end
-    enterpowersavings()    
+
+    conn = nil
+    sent = nil
+    
+    enterpowersavings()
 end)
