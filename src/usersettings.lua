970804027347bef25787680c39ab235c1fc0f08d
Michael Lucas <michael.lucas@kahosl.be>
2015-06-13 22:47:58 +0200

    Made TSL2561 gain user-configurable

--- src/usersettings.lua
+++ src/usersettings.lua
@@ -2,7 +2,7 @@
 SSID = "" 
 password = ""
 APIkey = "" -- thingspeak API write key for channel
-APIdelay = 120 -- thingspeak update internval in seconds (min: 15s)
+APIdelay = 30 -- thingspeak update internval in seconds (min: 15s)
 DHTPIN = 7 --  DHT22 data pin, GPIO13
 SDA_PIN = 5 -- lux sda pin, GPIO12
 SCL_PIN = 6 -- lux scl pin, GPIO14
@@ -14,6 +14,10 @@ lxSensor = 1
 --  0   :   13.7ms
 --  1   :   101ms
 --  2   :   402ms
-lxIntTime = 1
+lxIntTime = 2
+-- TSL2561 gain setting
+--  0   :   no gain
+--  16   :   16x
+lxGain = 0
 
 serialOut = true
