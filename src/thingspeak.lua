8d9667be6ddbc7c9302e2475fdfaa2a93a47805d
Michael Lucas <michael.lucas@kahosl.be>
2015-07-20 11:15:27 +0200

    Split packet creation from network handling

--- src/thingspeak.lua
+++ src/thingspeak.lua
@@ -9,6 +9,35 @@ local receivedReply = false
 local con
 local onFinished
 
+local function buildPacket()
+    -- construct fields string
+    local temp = {}  
+    table.insert(temp,"headers=false&field1="..(t).."&field2="..(h))
+    if lxSensor == 1 then
+        table.insert(temp,"&field3="..(lx0))
+    elseif lxSensor == 2 then
+        table.insert(temp,"&field3="..(lx0).."&field4="..(lx1))
+    end
+    if readV then
+        table.insert(temp,"&field5="..(Vdd))
+    end
+    local fields = table.concat(temp)
+    local length = string.len(fields)
+
+    -- construct http header and body
+    temp = {}
+    table.insert(temp, "POST /update HTTP/1.1\r\n")
+    table.insert(temp, "Host: api.thingspeak.com\r\n")
+    table.insert(temp, "X-THINGSPEAKAPIKEY: "..APIkey.."\r\n")
+    table.insert(temp, "Connection: close\r\n")
+    table.insert(temp, "Content-Type: application/x-www-form-urlencoded\r\n")
+    table.insert(temp, "Content-Length: "..length.."\r\n\r\n")
+    table.insert(temp, fields)
+    table.insert(temp, "\r\n\r\n")
+   
+    return table.concat(temp)
+end
+
 function M.init(callback)
     onFinished = callback
     con = net.createConnection(net.TCP, 0)
@@ -29,42 +58,17 @@ function M.init(callback)
         if (serialOut and (not receivedReply)) then
                 print("Didn't receive a reply.")
         end
-        --gotosleep()
         onFinished()
     end)
     con:on("connection", function(connection)
         if serialOut then 
             print("Connection succeeded")
         end
-        -- construct fields string
-        local temp = {}  
-        table.insert(temp,"headers=false&field1="..(t).."&field2="..(h))
-        if lxSensor == 1 then
-            table.insert(temp,"&field3="..(lx0))
-        elseif lxSensor == 2 then
-            table.insert(temp,"&field3="..(lx0).."&field4="..(lx1))
-        end
-        if readV then
-            table.insert(temp,"&field5="..(Vdd))
-        end
-        local fields = table.concat(temp)
-        local length = string.len(fields)
-
-        -- construct http header and body
-        temp = {}
-        table.insert(temp, "POST /update HTTP/1.1\r\n")
-        table.insert(temp, "Host: api.thingspeak.com\r\n")
-        table.insert(temp, "X-THINGSPEAKAPIKEY: "..APIkey.."\r\n")
-        table.insert(temp, "Connection: close\r\n")
-        table.insert(temp, "Content-Type: application/x-www-form-urlencoded\r\n")
-        table.insert(temp, "Content-Length: "..length.."\r\n\r\n")
-        table.insert(temp, fields)
-        table.insert(temp, "\r\n\r\n")
-        local post = table.concat(temp)
-
+        
         -- send the data packet
-        if(debug and serialOut) then print("Sending data:") print(post) end
-        connection:send(post, function(connection)
+        local packet = buildPacket()
+        if(debug and serialOut) then print("Sending data:") print(packet) end
+        connection:send(packet, function(connection)
             if serialOut then print("Sent data") sent = true end
         end)   
     end)
