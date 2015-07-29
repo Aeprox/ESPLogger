65dd7a3ecb370222a66458b55e2b21be08536713
Aeprox <aeprox@aeprox.net>
2015-07-29 20:39:52 +0200

    Updated readme

--- src/thingspeak/thingspeak_example.lua
+++ src/thingspeak/thingspeak_example.lua
@@ -6,7 +6,7 @@
 --
 -- MIT license, http://opensource.org/licenses/MIT
 -- ***************************************************************************
-wkey = "8T9YLMBFQEPSPZ1S"
+wkey = "8T9YUBFQEPSPZF5" -- thingspeak API write key
 dataValues = {} 
 
 wifi.sta.connect()
